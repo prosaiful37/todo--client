275ec36672401f96de573983de0a52991527ff5c
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-07-01 18:50:50 +0600

    fixed issue

--- src/pages/Home/AddList/AddList.js
+++ src/pages/Home/AddList/AddList.js
@@ -1,51 +1,31 @@
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
 import React from "react";
 
-const AddList = () => {
+const AddList = (list) => {
+ 
   return (
-    <div>
-      <div class="hero min-h-screen">
-        <div class="hero-content text-center">
-          <div class="max-w-md">
-          <div class="overflow-x-auto">
-  <table class="table w-full">
-    {/* <!-- head --> */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* <!-- row 1 --> */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* <!-- row 2 --> */}
-      <tr class="hover">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* <!-- row 3 --> */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-          </div>
+    
+    <div className="todo-list-item mt-5">
+    <div className="todo-item bg-white rounded p-5">
+        <p className="text-black">
+          <input className="mr-3 " type="checkbox" />
+          {list.item}{" "}
+          <span className="text-right m-5">
+            <button>
+              {" "}
+              <PencilAltIcon className="h-5 w-5 text-black" />
+            </button>
+          </span>
+          <span>
+            <button>
+              {" "}
+              <TrashIcon className="h-5 w-5 text-black" />
+            </button>
+          </span>
+        </p>
         </div>
       </div>
-    </div>
+    
   );
 };
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,60 +1,96 @@
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
 import React, { useEffect, useState } from "react";
-import {  PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
+// import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
 import AddList from "./AddList/AddList";
 
 const Home = () => {
-  const [itemText, setItemText] = useState('');
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/list")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
   //add to do item to database
 
-  const additem = async() =>{
-    try{
-      const res => await axios.post();
-    }
-    catch(err){
-      console.log(err);
-    }
-  }
+  const handleAddSubmit = (event) => {
+    event.preventDefault();
 
+    const item = event.target.list.value;
+
+    const list = { item };
+
+    //list add to server
+    const url = `http://localhost:5000/list`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(list),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <div className="bg-primary">
       <div class="hero min-h-screen ">
-        <div class="hero-content">
+        <div class="hero-content flex flex-col">
           <div class="drop-shadow-md bg-secondary p-5 rounded">
-            <form action="">
+            <form onSubmit={handleAddSubmit} action="">
               <label class="input-group text-black">
                 <input
+                  name="list"
                   type="text"
                   placeholder="Add item ..."
                   class="input input-bordered input-primary w-full max-w-xs"
-                  onChange={e => {setItemText(e.target.value)}} value={itemText}
                 />
                 <button class="btn btn-primary">Add</button>
               </label>
             </form>
 
-            <div className="todo-list-item mt-5">
-              <div className="todo-item bg-white rounded p-5">
             
+                
+              
+          </div>
+
+          <div className="drop-shadow-md bg-secondary p-5 rounded">
+          {
+            items.map(list => 
+            <div className="todo-list-item mt-5">
+            <div className="todo-item bg-white rounded p-5">
                 <p className="text-black">
-                <input className="mr-3 " type="checkbox" />
-                  This is item 1{" "}
+                  <input className="mr-3 " type="checkbox" />
+                  {list.item}{" "}
                   <span className="text-right m-5">
                     <button>
                       {" "}
-                      <PencilAltIcon className="h-5 w-5 text-black" /> 
+                      <PencilAltIcon className="h-5 w-5 text-black" />
                     </button>
                   </span>
                   <span>
                     <button>
                       {" "}
-                      <TrashIcon className="h-5 w-5 text-black" /> 
+                      <TrashIcon className="h-5 w-5 text-black" />
                     </button>
                   </span>
                 </p>
-              </div>
-            </div>
+                </div>
+              </div>)
+          }
           </div>
+
+          
+          {/* {items.map((list) => (
+                  <AddList list={list} key={list._id}></AddList>
+                ))} */}
+          
         </div>
       </div>
     </div>
